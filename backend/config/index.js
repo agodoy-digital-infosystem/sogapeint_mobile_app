c7a0455a06cade1d9e19c8a74d5a318f64551a49
agodoy-digital-infosystem <agodoy@digital-infosystem.fr>
2024-10-11 15:42:53 +0200

    backend/config/index.js

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,4 +1,10 @@
 // config/index.js
+/**
+ * Ce fichier combine les configurations d'environnement et de JWT
+ * pour les exporter en tant qu'objet de configuration unique.
+ * 
+ * Il est utilisé pour simplifier l'importation des configurations dans d'autres fichiers.
+ */
 
 const jwtConfig = require('./jwtConfig');
 const envConfig = require('./env');
@@ -6,4 +12,4 @@ const envConfig = require('./env');
 module.exports = {
     ...jwtConfig,
     ...envConfig,
-};
+}; // Export the merged configuration object
