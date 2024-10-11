588f43dd434e74e38ef1f0858689f19e75907a5c
agodoy-digital-infosystem <agodoy@digital-infosystem.fr>
2024-10-11 15:57:55 +0200

    backend/config/jwtConfig.js

--- backend/config/jwtConfig.js
+++ backend/config/jwtConfig.js
@@ -1,4 +1,8 @@
+/**
+ * This file contains the JWT configuration object.
+ */
+
 module.exports = {
     secret: process.env.JWT_SECRET,
     expiresIn: '1h',
-};
+}; // Export the JWT configuration object
