1bb8884a03e61b290213fc242cdd6c0825eda3ee
agodoy-digital-infosystem <agodoy@digital-infosystem.fr>
2024-10-11 22:28:42 +0200

    backend/src/routes/signatureRoutes.js

--- backend/src/routes/signatureRoutes.js
+++ backend/src/routes/signatureRoutes.js
@@ -1,3 +1,6 @@
+/**
+ * Ce fichier représente le point d'entrée des routes pour les signatures électroniques.
+ */
 const express = require('express');
 const SignatureController = require('../controllers/signatureController');
 const { authenticateToken } = require('../middlewares/authMiddleware');
