5ea14cef7e86ab973c78ea4a01a3159983ec84ad
agodoy-digital-infosystem <agodoy@digital-infosystem.fr>
2024-10-11 20:28:28 +0200

    backend/src/models/userModel.js

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,3 +1,8 @@
+/**
+ * Ce modèle définit le schéma de la table des utilisateurs dans la base de données.
+ * 
+ * Il est utilisé pour effectuer des opérations de base de données sur la table des utilisateurs.
+ */
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
