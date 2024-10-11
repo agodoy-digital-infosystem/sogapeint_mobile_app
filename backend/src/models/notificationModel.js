fe5cfbae26195a4b2d88510348a4e2fb380c083d
agodoy-digital-infosystem <agodoy@digital-infosystem.fr>
2024-10-11 19:43:22 +0200

    backend/src/models/notificationModel.js

--- backend/src/models/notificationModel.js
+++ backend/src/models/notificationModel.js
@@ -1,3 +1,8 @@
+/**
+ * Ce modèle définit le schéma de la table des notifications dans la base de données.
+ * 
+ * Il est utilisé pour effectuer des opérations de base de données sur la table des notifications.
+ */
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../../config/database');
 
